28d248eed8d48db6b6112f277cd4e593ef86ba25
Israel Moreira de Souza <israelmoreira1230@gmail.com>
2023-05-12 20:48:09 -0300

    first commit

---
+++ src/test/java/com/israelsouza1230/site/SiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israelsouza1230.site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
